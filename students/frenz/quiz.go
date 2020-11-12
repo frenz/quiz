15db0c695d463308b2e354948421f6eab5fbff5c
Giuseppe <giuseppe@techtribe.nl>
2020-11-12 16:19:53 +0100

    quiz first part solution

--- students/frenz/quiz.go
+++ students/frenz/quiz.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
+type problemData struct {
+	Question string
+	Answer   string
+}
+
+func printError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+func readCsvLines(filename string) [][]string {
+	csvFile, err := os.Open(filename)
+	printError(err)
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	printError(err)
+	return csvLines
+}
+func parseLinesToProblems(csvLines [][]string) []problemData {
+	var problems []problemData
+	for _, line := range csvLines {
+		problem := problemData{
+			Question: line[0],
+			Answer:   line[1],
+		}
+		problems = append(problems, problem)
+	}
+	return problems
+}
+
+func askUser(problems []problemData) int {
+	correct := 0
+	for _, problem := range problems {
+		answer := ""
+		fmt.Printf("%s ?\n", problem.Question)
+		fmt.Scanf("%s", &answer)
+		if answer == problem.Answer {
+			correct++
+		}
+	}
+	return correct
+}
 func main() {
-	fmt.Println("vim-go")
+	filename := "problems.csv"
+	csvLines := readCsvLines(filename)
+	problems := parseLinesToProblems(csvLines)
+	correct := askUser(problems)
+	fmt.Printf("User answered %d correct %d not correct", correct, len(problems)-correct)
 }
